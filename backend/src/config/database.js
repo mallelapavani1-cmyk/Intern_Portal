59bab28c9650c01cf59e23443d143c00ad30a38e
Anupam Sharma <anupamsharma9786@gmail.com>
2026-09-01 09:30:59 +0530

    Revert "update dashboards and application changes"

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,15 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import dns from "dns";
 
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({
-    path: path.resolve(__dirname, "../../.env")
-});
+dotenv.config();
 
 dns.setServers([
   "8.8.8.8",
